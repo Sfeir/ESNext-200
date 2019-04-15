301a5e4fe752315e1b4beb80221f3808721d67d3
tonygoussot <tonygoussot@users.noreply.github.com>
2019-04-15 18:58:53 +0200

    add functional js exercice

---
+++ exercises/functional/exo.js
@@ -0,0 +1,18 @@
+let shoppingCart = [
+  { productTitle: "Functional Programming", type: "books", amount: 10 },
+  { productTitle: "Kindle", type: "eletronics", amount: 30 },
+  { productTitle: "Shoes", type: "fashion", amount: 20 },
+  { productTitle: "Clean Code", type: "books", amount: 60 }
+];
+
+const byBooks = (order) => /**/;
+const getAmount = (order) => /**/;
+const sumAmount = (acc, amount) => /**/;
+
+function getTotalAmount() {
+  return /**/
+}
+
+const totalAmount = getTotalAmount(shoppingCart);
+
+module.exports = totalAmount;
