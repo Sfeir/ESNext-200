b283feba586eed6d4be85a356a210e4ba9aec3fd
tonygoussot <tonygoussot@users.noreply.github.com>
2019-04-15 18:59:39 +0200

    add functional js solution

--- exercises/functional/exo.js
+++ exercises/functional/exo.js
@@ -1,18 +1,21 @@
 let shoppingCart = [
-  { productTitle: "Functional Programming", type: "books", amount: 10 },
-  { productTitle: "Kindle", type: "eletronics", amount: 30 },
-  { productTitle: "Shoes", type: "fashion", amount: 20 },
-  { productTitle: "Clean Code", type: "books", amount: 60 }
+  {productTitle: "Functional Programming", type: "books", amount: 10},
+  {productTitle: "Kindle", type: "eletronics", amount: 30},
+  {productTitle: "Shoes", type: "fashion", amount: 20},
+  {productTitle: "Clean Code", type: "books", amount: 60}
 ];
 
-const byBooks = (order) => /**/;
-const getAmount = (order) => /**/;
-const sumAmount = (acc, amount) => /**/;
+const byBooks   = (order) => order.type === 'books';
+const getAmount = (order) => order.amount;
+const sumAmount = (acc, amount) => acc + amount;
 
-function getTotalAmount() {
-  return /**/
+function getTotalAmount (shoppingCart) {
+  return shoppingCart
+    .filter(byBooks)
+    .map(getAmount)
+    .reduce(sumAmount, 0)
 }
 
 const totalAmount = getTotalAmount(shoppingCart);
 
-module.exports = totalAmount;
+module.exports = {totalAmount};
